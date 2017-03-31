d36f2bf8a3ce9032e6ed0907d6fb5e83bbb29651
ha-li <ha-li@gmail.com>
2017-03-30 22:54:19 -0700

    add connection pool

--- jms/src/main/java/com/gecko/message/repository/ProducerRepository.java
+++ jms/src/main/java/com/gecko/message/repository/ProducerRepository.java
@@ -3,6 +3,7 @@ package com.gecko.message.repository;
 import com.gecko.message.producer.AbstractProducer;
 import com.gecko.message.producer.Producer;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.pool.PooledConnectionFactory;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -30,6 +31,9 @@ public class ProducerRepository {
       ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin", "admin", nioConnection);
       connectionMap.put ("connectionKey", connectionFactory);
 
+      PooledConnectionFactory pooledFactory = new PooledConnectionFactory(connectionFactory);
+      connectionMap.put("pooledConnectionKey", pooledFactory);
+
       // fake a database, repository is the db
       // TODO: replace with hibernate
       producerRepository.put (
@@ -39,7 +43,7 @@ public class ProducerRepository {
                                   "Gecko.global.{env}.test.Queue",
                                   "org.apache.activemq.command.ActiveMQQueue",
                                   "com.gecko.message.producer.DefaultProducer",
-                                  "connectionKey") );
+                                  "pooledConnectionKey") );
    }
 
    public static AbstractProducer findProducer(String accessKey) throws Exception {
