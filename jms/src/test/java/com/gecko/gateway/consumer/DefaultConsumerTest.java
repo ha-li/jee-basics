cbdb87059ea0f1966f59b32bcd09b506fad2c1bc
ha-li <ha-li@gmail.com>
2017-04-03 20:38:14 -0700

    add unit test

---
+++ jms/src/test/java/com/gecko/gateway/consumer/DefaultConsumerTest.java
@@ -0,0 +1,43 @@
+package com.gecko.gateway.consumer;
+
+import org.apache.activemq.command.ActiveMQBlobMessage;
+import org.apache.activemq.command.ActiveMQMessage;
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * Created by hlieu on 04/3/17.
+ */
+public class DefaultConsumerTest {
+
+   @Test
+   public void test_on_message () throws Exception {
+      ActiveMQTextMessage message = new ActiveMQTextMessage ();
+      message.setStringProperty ("TransactionId", UUID.randomUUID ().toString());
+      message.setText("A random test message");
+
+      DefaultConsumer consumer = new DefaultConsumer ();
+      consumer.onMessage (message);
+   }
+
+   @Test
+   public void test_blob_message () throws Exception {
+      ActiveMQMessage message = new ActiveMQBlobMessage ();
+      message.setStringProperty ("TransactionId", UUID.randomUUID ().toString());
+
+      DefaultConsumer consumer = new DefaultConsumer ();
+      consumer.onMessage (message);
+   }
+
+   @Test
+   public void test_exception () throws Exception {
+      ActiveMQTextMessage message = new ActiveMQTextMessage ();
+      message.setText("do nothing");
+
+      DefaultConsumer consumer = new DefaultConsumer ();
+      consumer.onMessage (null);
+   }
+
+}
